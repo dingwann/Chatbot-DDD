d771502d35d56ff0e89cc0bb4e76b888f21bec7f
wangcai <catchskatecc@gmail.com>
2025-12-20 02:00:36 +0800

    initialize project😀

---
+++ chatbot-ddd-interfaces/src/main/java/org/example/chatbot/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package org.example.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangcai
+ * @description App启动入口
+ * @date 2025/12/20
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ chatbot-ddd-interfaces/src/test/java/org/example/chatbot/api/ApiTest.java
@@ -0,0 +1,9 @@
+package org.example.chatbot.api;
+
+/**
+ * @author wangcai
+ * @description 单元测试
+ * @date 2025/12/20
+ */
+public class ApiTest {
+}
